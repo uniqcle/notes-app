7691867587d28b0c5bd3f88912f7e9bbe5c3455d
uniqcle <uniqcle@yandex.ru>
2021-05-21 16:03:56 +0700

    js code start

---
+++ script.js
@@ -0,0 +1,9 @@
+const notesEl = document.querySelector('.notes'); 
+const editBtn = document.querySelector('.edit'); 
+const deleteBtn = document.querySelector('.delete'); 
+const main = notesEl.querySelector('.main'); 
+const textArea = notesEl.querySelector('textarea'); 
+
+editBtn.addEventListener('click', ()=>{
+
+}); 
